e883eeb6d0f2e69fd8db136db19052caddc23327
basvancleef1 <basvancleef@icloud.com>
2024-02-10 22:49:07 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Bas van Cleef on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
